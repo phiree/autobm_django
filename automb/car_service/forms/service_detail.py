27a3295a0d36cb5659103267e65894784d180127
phiree <phiree@gmail.com>
2014-08-08 07:05:13 +0800

    dddddd

---
+++ automb/car_service/forms/service_detail.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from ..models import ServiceDetail,Service
+__author__ = 'Administrator'
+from django import forms
+
+class ServiceDetailForm(ModelForm):
+    class Meta:
+        model=ServiceDetail
+        exclude=('service',)
+
+class ServiceForm(ModelForm):
+    class Meta:
+        model=Service
