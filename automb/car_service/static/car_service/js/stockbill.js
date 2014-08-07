1a35644f5e7b2558f22bfc4f18afaa2ef718eb90
phiree <phiree@gmail.com>
2014-08-08 05:25:28 +0800

    Revert "init"
    This reverts commit 18c89dbc3fd0351d7860c5e0a6dd1c3eb93d0ff9.

--- automb/car_service/static/car_service/js/stockbill.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-            //$("#billeditor").tabs();
-            $("#btn_save").click(function(){
-            	$("#fm_detail_update").submit();
-            });
-            $('#btn_apply').confirmation();
-        });
