7f01144578c1f026c8534a084343f615b58d81d6
phiree <phiree@gmail.com>
2014-11-28 17:03:25 +0800

    change password

--- automb/automb/urls.py
+++ automb/automb/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-
+from django.contrib.auth import views
 admin.autodiscover()
 import permission; permission.autodiscover()
 urlpatterns = patterns(''
@@ -12,6 +12,12 @@ urlpatterns = patterns(''
 
     ,url(r'^admin/', include(admin.site.urls))
     ,url(r'^profile/',include('userprofile.urls',namespace='userprofile'))
+    ,url(r'^profile/change_password/$',views.password_change,
+        {'template_name':'reset_password.html'},
+                         name='password_change'),
+                     url(r'^profile/change_password_done/$',views.password_change_done,
+                       {'template_name':'password_change_successfully.html'},
+                         name='password_change_done')
     ,url(r'',include('car_service.urls',namespace='car_service'))
     ,
 )+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- automb/userprofile/models.py
+++ automb/userprofile/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
     user=models.OneToOneField(User)
+    real_name=models.CharField(max_length=20,null=True,blank=True,verbose_name='真实姓名')
+    gender_choice=(('m','男'),('f','女'))
+    gender=models.CharField(max_length=2,choices=gender_choice,default=gender_choice[0][0],verbose_name='性别')
     phone=models.CharField(max_length=200,null=True,verbose_name='电话号码')
-
+    email=models.EmailField(max_length=100,null=True,blank=True,verbose_name='电子邮件')
 User.profile=property(lambda u:UserProfile.objects.get_or_create(user=u)[0])
