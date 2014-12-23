6ca2ea64b5b868752f507261ad8779d714401c92
phiree <phiree@gmail.com>
2014-12-23 21:34:53 +0800

    middleware change

--- automb/car_service/middleware.py
+++ automb/car_service/middleware.py
@@ -2,9 +2,17 @@ from django.conf import settings
 
 class MobileMiddleware(object):
     def process_request(self, request):
+        settings.TEMPLATE_DIRS = settings.TEMPLATE_DIRS_DESKTOP
         subdomain = request.META.get('HTTP_HOST', '').split('.')
         if 'm' in subdomain:
             settings.TEMPLATE_DIRS = settings.TEMPLATE_DIRS_MOBILE
+            return
         else:
-            settings.TEMPLATE_DIRS = settings.TEMPLATE_DIRS_DESKTOP
-        settings.TEMPLATE_DIRS = settings.TEMPLATE_DIRS_MOBILE
+            #for test
+            port=request.META.get('SERVER_PORT')
+            if port=='8000':
+                settings.TEMPLATE_DIRS = settings.TEMPLATE_DIRS_MOBILE
+            else:
+                settings.TEMPLATE_DIRS = settings.TEMPLATE_DIRS_DESKTOP
+
+
